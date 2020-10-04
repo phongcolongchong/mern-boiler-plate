b3bf149667286c73dc5593f60aec879fa4a86899
phongcolongchong <57311841+phongcolongchong@users.noreply.github.com>
2020-10-04 09:07:10 +0700

    deploy server to heroku

--- server/index.js
+++ server/index.js
@@ -20,6 +20,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+app.get('/', (req, res) => {
+  res.json({
+    'Hello': 'I am Phong :D'
+  });
+});
+
 app.get('/api/user/auth', auth, (req, res) => {
   res.status(200).json({
     _id: req._id,
